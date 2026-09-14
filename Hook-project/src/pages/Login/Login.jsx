b266ff8086bce4c8b9c99018d199b26e35fd1b98
mi-sami <minhazsami446@gmail.com>
2026-09-14 11:22:20 +0600

    Login page almost done

--- Hook-project/src/pages/Login/Login.jsx
+++ Hook-project/src/pages/Login/Login.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "../../Components/Navbar";
 import { useForm } from "react-hook-form";
+import Data from "../../Data/Data.json"
 
 const Login = () => {
   const {
@@ -10,9 +11,28 @@ const Login = () => {
     formState: { errors },
   } = useForm()
 
-  const onSubmit = (data) => console.log(data)
+  const onSubmit = (data) => ProcessLogin(data)
 
   console.log(watch("example"))
+
+
+  const ProcessLogin = (formdata)=>{
+    const fetchData = async()=>{
+      try{
+        const tempUser = Data.find(user => user.Email === formdata.Email && user.Password === formdata.Password) || {};
+
+        console.log(tempUser);
+        
+      }
+      catch(err){
+        console.log(err);
+        
+      }
+
+    }
+
+    fetchData();
+  }
 
   return (<>
     <Navbar />
@@ -42,24 +62,15 @@ const Login = () => {
       <input className="border border-s-sky-600 p-2 rounded-2xl" type="password" placeholder="Enter Your Password"  {...register("Password", { required: true })} />
 
       <br />
-      
+
 
       {errors.Password && <span className="text-red-700">This password is required</span>}
       </div>
 
 
-    
 
-      
-      
-      
-      
-      
-
-
-
-      <div className="mt-2 w-[90px] mx-auto">
-      <input className="bg-green-900 text-white p-2 rounded" type="submit" />
+      <div className="mt-4 w-[90px] mx-auto">
+      <input className="bg-green-700 text-white p-2 rounded-md px-4 hover:bg-green-900 hover:text-white" type="submit" />
       </div>
     </form>
     </div>
